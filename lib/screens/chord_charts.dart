bc318d9a106957e3e58d18babef9d37a73c5816a
Nathan Whitney <natewhit205@gmail.com>
2024-12-09 21:24:49 +0000

    Dynamically generate Chord Chart Screen

--- lib/screens/chord_charts.dart
+++ lib/screens/chord_charts.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 import 'package:flutter_music_application/colors.dart';
-import 'package:flutter_music_application/main.dart';
-import 'package:flutter_music_application/saved_chord_progression.dart';
-import 'package:flutter_music_application/styles.dart';
-import 'package:flutter_music_application/widgets/button.dart';
+import 'package:flutter_music_application/widgets/chart_row.dart';
 
 class ChordChartsScreen extends StatefulWidget {
   const ChordChartsScreen({super.key, selectedKey});
@@ -17,31 +14,12 @@ class ChordChartsScreen extends StatefulWidget {
 
 class ChordChartsScreenState extends State<ChordChartsScreen> {
 
-  String _getDisplayString(SavedChordProgression chordProgression) => chordProgression.chordProgression.join(' | ');
-
-  double _calculateFontSize(int length) {
-    double output;
-    double minFont = 15;
-    double maxFont = 18;
-    int lowerBound = 8;
-    int upperBound = 32;
-
-    if (length <= 8) {
-      output = maxFont;
-    } else {
-      output = maxFont - (length - lowerBound) * (maxFont - minFont) / (upperBound - lowerBound);
+  List<Widget> _getCharts() {
+    List<Widget> output = [];
+    for (int i = 1; i <= 7; i++) {
+      output.add(ChartRow(interval: i));
     }
     return output;
-  }
-  
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
   }
 
   @override
@@ -52,114 +30,13 @@ class ChordChartsScreenState extends State<ChordChartsScreen> {
         title: const Text('Music App Name'),
         backgroundColor: AppTheme.primary,
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'I',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'ii',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'iii',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'IV',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'V',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'vi',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 50,
-                  child: Text(
-                    'vii°',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyle.bold(context, color: Colors.black),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      body: FadingEdgeScrollView.fromScrollView(
+        gradientFractionOnStart: 0.3,
+        gradientFractionOnEnd: 0.3,
+        child: ListView(
+          controller: ScrollController(),
+          children: _getCharts(),
+        ),
       ),
     );
   }
--- lib/widgets/chart_row.dart
+++ lib/widgets/chart_row.dart
@@ -1,76 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_music_application/colors.dart';
+import 'package:flutter_music_application/styles.dart';
 
-class CustomMaterialButton extends StatelessWidget {
-  final void Function()? onPressed;
-  final Widget? child;
+class ChartRow extends StatelessWidget {
+  final int interval;
 
   // Constant Values
-  final Color? color = AppTheme.secondaryAccent;
-  final ShapeBorder? shape = const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-  );
-  final Color? textColor = Colors.white;
-  final double? height = 30;
-  final double? minWidth = 100;
-  final EdgeInsetsGeometry? padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 8);
+  final Map<int, String> intervalMap = {
+    1: 'I',
+    2: 'ii',
+    3: 'iii',
+    4: 'IV',
+    5: 'V',
+    6: 'vi',
+    7: 'vii°'
+  };
 
-  const CustomMaterialButton({
+  final double? width = 50;
+  final EdgeInsetsGeometry padding = const EdgeInsets.fromLTRB(8.0, 32.0, 8.0, 32.0);
+  final MainAxisAlignment mainAxisAlignment = MainAxisAlignment.spaceBetween;
+
+  ChartRow({
     super.key,
-    required this.onPressed,
-    this.child
+    required this.interval,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: minWidth,
-      child: MaterialButton(
-        onPressed: onPressed,
-        color: color,
-        shape: shape,
-        textColor: textColor,
-        height: height,
-        minWidth: minWidth,
-        padding: padding,
-        child: child,
-      )
-    );
-  }
-}
-
-class SimpleActionButton extends StatelessWidget {
-  final void Function()? onPressed;
-  final Widget? child;
-  final Color? color;
-
-  // Constant Values
-  final ShapeBorder? shape = const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-  );
-  final Color? textColor = Colors.white;
-  final double? height = 45;
-  final double? minWidth = 130;
-  final EdgeInsetsGeometry? padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 8);
-
-  const SimpleActionButton({
-    super.key,
-    required this.onPressed,
-    required this.color,
-    required this.child
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: onPressed,
-      color: color,
-      shape: shape,
-      textColor: textColor,
-      height: height,
-      minWidth: minWidth,
+    return Padding(
       padding: padding,
-      child: child,
+      child: Row(
+        mainAxisAlignment: mainAxisAlignment,
+        children: [
+          SizedBox(
+            width: width,
+            child: Text(
+              intervalMap[interval]!,
+              textAlign: TextAlign.center,
+              style: AppTextStyle.bold(context, color: Colors.black),
+            ),
+          ),
+          Image.asset(
+            'assets/chord_imgs/F/1.png',
+            width: MediaQuery.of(context).size.width / 3,
+          ),
+          Image.asset(
+            'assets/chord_imgs/F/4.png',
+            width: MediaQuery.of(context).size.width / 3,
+          ),
+        ],
+      ),
     );
   }
 }
